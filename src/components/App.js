d9cfe53d876eeb86ccad4efe173cab78bb8d87b9
Elise Erickson <elisece717@gmail.com>
2023-12-27 15:24:57 -0500

    adds required functionality and passes tests

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,15 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  //const appClass = false ? "App dark" : "App light"
+  const [appClass, setClass] = React.useState("App light")
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button 
+        onClick={() => setClass( appClass => appClass === "App dark" ? "App light" : "App dark")}>
+          {appClass === "App dark" ? "Dark Mode" : "Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
